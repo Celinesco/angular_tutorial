4b50dec8b3946c6b74af218af22c197a71c384f9
celines <celina.alsina@gmail.com>
2023-08-08 14:45:36 +0200

    Implemented routerLink to use dinamical parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
