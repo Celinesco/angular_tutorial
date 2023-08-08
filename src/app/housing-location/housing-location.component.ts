7f86558fb7b3add9afe96ce2358f2f9a2b390205
celines <celina.alsina@gmail.com>
2023-08-08 13:37:15 +0200

    Added dynamic values to templates

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,13 +1,31 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
-  template: ` <p>housing-location works!</p> `,
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
+  `,
   styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
-  @Input() housingLocation!: HousingLocationComponent;
+  @Input() housingLocation!: HousingLocation;
 }
+
+// by mistake I wrote HousingLocationComponent instead pf HousingLocation next to housingLocation! and I got this error:
+// src/app/home/home.component.ts:19:10 - error TS2741: Property 'housingLocation' is missing in type 'HousingLocation' but required in type 'HousingLocationComponent'.
+
+// 19         [housingLocation]="housingLocation"
